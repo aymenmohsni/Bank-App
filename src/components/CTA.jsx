88a05310af2b23146433ed4565d8d6c501df71c8
Aymen Mohsni <aymen.mohsni97@gmail.com>
2024-01-12 14:42:58 +0100

    Call To Action section added

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import styles from "../style";
+import Button from "./Button";
 
-const CTA = () => {
-  return (
-    <div>CTA</div>
+const CTA = () => (
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className="flex-1 flex flex-col">
+        <h2 className={styles.heading2}>Let's try our service now!</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium incidunt quod tenetur reiciendis, natus rem, asperiores ex explicabo officia officiis odit optio, ipsa harum amet minima maiores magni facere quo!</p>
+      </div>
+
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+        <Button />
+      </div>
+    </section>
   )
-}
+
 
 export default CTA
